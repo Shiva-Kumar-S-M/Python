76f89d39b777e37b0aa6df6455eba8779f844a9c
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-01-27 16:46:26 +0530

    type conversion1

--- Basic/type_conversion.py
+++ Basic/type_conversion.py
@@ -21,5 +21,69 @@
 # string = "Python's for beginners "
 # print(string[0:])
 
-name= "Shivakumar"
-print(name[1:-1])     #excludes last character
+# name= "Shivakumar"
+# print(name[1:-1])     #excludes last character
+
+
+#formatted string
+
+# first='shivu'
+# last='kumar'
+# msg= first + '[' + last +'] is a coder'
+# formatted=f'{first} [{last}] is a coder'
+# print(msg)
+# print(formatted)
+
+#String methods len() , Upper(),lower(),find(),replace(),in()
+
+# Player="Virat is my favorite"
+# print(len(Player))
+# print(Player.upper())
+# print(Player.lower())
+# print(Player.find('Virat'))            #return array of index
+# print(Player.replace('favorite','Inspiration'))
+# print('Virat' in Player)   #boolean
+
+
+#arithmetic operators +,-,*,/,//,%,**(pow)
+# print(10+3)
+
+# x=25
+# x=x+5
+# print(x)
+
+# x*=5  #augmented arithmetic operator
+# print(x)
+
+
+
+#operator precedence 
+# x=10+3*2
+# print(x)
+
+#parenthesis()->exponential(2**3)->Multiplication/division->addition/subtraction
+
+
+#math functions
+# import math
+# print(math.ceil(2.9))
+# print(math.floor(2.9))
+
+
+ #if statements
+# price=1000000
+# is_goodCredit=True
+
+# if is_goodCredit:
+#     down_payment=0.1*price
+# else:
+#     down_payment=0.2*price
+
+# print(f"Down Payment: ${down_payment}")
+
+# age=int(input("Enter your age:"))
+
+# if(age>18):
+#     print("Voter is eligible")
+# else:
+#     print("Go kiddo your still not eligible")
