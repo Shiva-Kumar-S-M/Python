1a593073245e528aae3b33bf150b1a823947a65c
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-01-27 13:30:05 +0530

    type conversion

---
+++ Basic/type_conversion.py
@@ -0,0 +1,25 @@
+# birth=input("What's your birth year:")
+# print(type(birth))
+# age=2025-int(birth)
+# print(type(age))
+# print(age)
+
+
+# weight = input("What is your Wieght in pounds:")
+# kg=int(weight)*0.45
+# print(kg)
+
+
+# String---- 
+
+# string = "Python's for beginners "
+# print(string)
+
+# string = 'Pythons for "beginners" '
+# print(string)
+
+# string = "Python's for beginners "
+# print(string[0:])
+
+name= "Shivakumar"
+print(name[1:-1])     #excludes last character
