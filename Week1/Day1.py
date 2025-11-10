e392ae697be7772c284b4e78345f0b60ed7caa8a
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-11-10 18:09:56 +0530

    Arrays

--- Week1/Day1.py
+++ Week1/Day1.py
@@ -1,1 +1,22 @@
-#Today I am learining about variables in Python
+#Today I am learining about Arrays in Python
+# cars=['BMW','Volvo','Ford']
+# x=cars[0]
+# print(x)
+
+# cars[0]="Shivu"
+# print(cars)
+# print(len(cars))
+
+cars=['BMW','Volvo','Ford']
+#Append Function adds an element at the end of the list
+cars.append("Honda")
+for x in cars:
+    print(x)
+
+#pop() removes the last element from the list 
+cars.pop()
+print(cars)
+
+#Remove() removes the specified element from the list 
+cars.remove("Ford")
+print(cars)
