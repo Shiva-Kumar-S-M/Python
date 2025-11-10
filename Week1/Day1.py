3a9cafe145d44716270b71a60eedbf854b6fc52d
Shiva-Kumar-S-M <shivukumar.iu@gmail.com>
2025-11-10 18:25:40 +0530

    List

--- Week1/Day1.py
+++ Week1/Day1.py
@@ -7,16 +7,35 @@
 # print(cars)
 # print(len(cars))
 
-cars=['BMW','Volvo','Ford']
-#Append Function adds an element at the end of the list
-cars.append("Honda")
-for x in cars:
-    print(x)
+# cars=['BMW','Volvo','Ford']
+# #Append Function adds an element at the end of the list
+# cars.append("Honda")
+# print(type(cars))
+# for x in cars:
+#     print(x)
 
-#pop() removes the last element from the list 
-cars.pop()
-print(cars)
+# #pop() removes the last element from the list 
+# cars.pop()
+# print(cars)
 
-#Remove() removes the specified element from the list 
-cars.remove("Ford")
-print(cars)
+# #Remove() removes the specified element from the list 
+# cars.remove("Ford")
+# print(cars)
+
+# num=[5,9,7,5,3,1,2,4,5,8,4,1,2,3]
+# num.sort()
+# print(num)
+# num.reverse()
+# print(num)
+# num.index(5)
+# print(num)
+
+
+#List is coleection which is ordered and changeable.Allows duplicate numbers
+# name=["Shivu","Mahesh","ravi"]
+# print(name)
+# print(len(name))
+
+num=list(("One","Two","Three","Four","Five"))
+print(num)
+print(type(num))
